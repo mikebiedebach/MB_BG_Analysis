5c9d9ab0f02313c57eb37f56dbdbdd249c5663b1
Michael Biedebach <mbiedebach@csumb.edu>
2025-09-02 16:00:10 -0700

    Update packages.py

--- src/packages.py
+++ src/packages.py
@@ -2,12 +2,7 @@ import pandas as pd
 import numpy as np
 
 def check_for_missing_columns(df_i, col_list):
-  '''
-  assigns null values to temp, humidty, dew point when HMP155 is turned off
-  '''
   missing_cols = set(col_list).difference(set(df_i.columns.unique()))
-
   for col in missing_cols:
     df_i[col]=np.nan
-
   return df_i
