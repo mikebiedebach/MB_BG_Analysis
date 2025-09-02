2b81f4ecc61159d43d2b5f81795563ac54015c9e
Michael Biedebach <mbiedebach@csumb.edu>
2025-09-02 15:20:54 -0700

    Delete packages.py

--- packages.py
+++
@@ -1,9 +0,0 @@
-import math
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.dates import DateFormatter
-from matplotlib import ticker
-import seaborn as sns
-import matplotlib.dates as mdates
-import statistics
